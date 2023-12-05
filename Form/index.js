f857e2607e9425cabdfc4bef35af21348a0ad939
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-12-05 13:06:48 +0700

    Event onFocus

--- Form/index.js
+++ Form/index.js
@@ -7,13 +7,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const maxChar = document.getElementById("inputNama").maxLength;
 
     const restChar = maxChar - typeChar;
-    document.getElementById("sisaKarakter").innerText = restChar;
+    document.getElementById("sisaKarakter").innerText = restChar.toString();
 
     restChar === 0
       ? (document.getElementById("sisaKarakter").innerText =
           "Batas Maksimal Karakter Sudah Terpenuhi")
       : restChar <= 5
       ? (document.getElementById("notifikasiSisaKarakter").style.color = "red")
-      : (document.getElementById("notifikasiSisaKarakter").style.color = black);
+      : (document.getElementById("notifikasiSisaKarakter").style.color = "black");
+  });
+
+  document.getElementById("inputNama").addEventListener("focus", () => {
+    console.log('inputNama: focus');
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "visible";
   });
 });
